ac8621924a627d04b95e43ad216440b81c716dda
haran2001 <hari.ayapps@gmail.com>
2021-05-04 12:20:43 +0530

    newton's cradle template

--- test.glsl
+++ test.glsl
@@ -4,4 +4,16 @@ float sdSphere(in vec3 p, in float r){
     return length(p)-r;
 }
 
+vec4 map(in vec3 p, float time){
+    float d = sdSphere(p, 0.2);
+    return vec4(d, p);
+}
 
+#define ZERO min(iFrame, 0)
+
+vec3 calcNormal(in vec3 pos, in float time){
+    vec3 n = vec3(0.0);
+    for(int i=ZERO; i<4; i++){
+        
+    }
+}
